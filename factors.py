ffdfeb07fb6f0902cf9d7bae82492d3147471ad2
sirln <siro@sirlawren.com>
2023-07-02 21:13:21 +0000

    RSA Factoring Challenge: Task 0. Factorize all the things!

---
+++ factors.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+'''
+My Factorization module
+'''
+import sys
+
+
+def factorize(n):
+    '''
+    factorizes a number into a product of 2 smaller numbers
+
+    Arguments
+    -------
+    n : int
+        number to be factorized
+    '''
+    i = 2
+
+    if n < 2:
+        return
+    while (i * i <= n):
+        if not (n % i):
+            j = n // i
+            print(f"{n}={j}*{i}")
+            return
+        i += 1
+
+def read_file():
+    '''
+    reads a file passed into the program
+
+    Raises
+    ------
+    FileNotFoundError
+        if file passed as argument 1 is not available / not found.
+    '''
+    if len(sys.argv) != 2:
+        print("Usage: factors <file>")
+        return
+    file_name = sys.argv[1]
+    try:
+        with open(file_name, 'r') as file:
+            for line in file:
+                num = int(line.strip())
+                factorize(num)
+    except FileNotFoundError:
+        print(f"File {file_name} not found!")
+
+if __name__ == '__main__':
+    read_file()
